562b817685859084b243674b0669a5c61b650a67
Raphael Pepiton Isla <rpbisla0025@gmail.com>
2025-09-03 16:12:58 +0800

    Components added

--- src/Components/SiteComponents/Footer.tsx
+++ src/Components/SiteComponents/Footer.tsx
@@ -12,7 +12,7 @@ import { ClockIconCustom, MailIcon, PinIcon, PhoneIcon } from '../Icons';
 export default function Footer() {
     const pathname = usePathname()
 
-    const shouldHideNavbar = pathname?.startsWith('/forms') || pathname?.startsWith('/admissions') || pathname?.startsWith('/login') || pathname?.startsWith('/enterprise-portal') || pathname?.startsWith('/feedback') ;
+    const shouldHideNavbar = pathname?.startsWith('/Tester') || pathname?.startsWith('/forms') || pathname?.startsWith('/admissions') || pathname?.startsWith('/login') || pathname?.startsWith('/enterprise-portal') || pathname?.startsWith('/feedback') ;
 
     if (shouldHideNavbar) {
         return null; // Don't render anything if conditions are met
