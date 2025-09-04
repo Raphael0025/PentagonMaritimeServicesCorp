8aefee0bc0b9974991de77b1029515c05270e9b4
Raphael Pepiton Isla <rpbisla0025@gmail.com>
2025-09-04 11:40:29 +0800

    ui

--- src/Components/ui/tracing-beam.tsx
+++ src/Components/ui/tracing-beam.tsx
@@ -49,7 +49,7 @@ export const TracingBeam = ({
   return (
     <motion.div
       ref={ref}
-      className={"relative mx-auto h-full w-full max-w-7xl px-6"}
+      className={"relative h-full w-full max-w-7xl px-6"}
     >
       <div className="absolute top-3 -left-4 md:-left-20">
         <motion.div
