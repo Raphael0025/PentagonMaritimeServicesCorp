79bd2433b12747be0abb406e1dc9197ab2828f93
Raphael0025 <rpbisla0025@gmail.com>
2025-10-19 17:05:23 +0800

    attendance form

--- src/Components/Page/Forms/PreviewAttendance.tsx
+++ src/Components/Page/Forms/PreviewAttendance.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Image from 'next/Image'
+import Image from 'next/image'
 import React from 'react';
 import { useState, useRef } from 'react'
 import { Box, Text, Input, useToast, Button, Grid, GridItem } from '@chakra-ui/react'
