735582f2dfda1fcd50fd4568ad8db57407f31a42
Raphael Pepiton Isla <rpbisla0025@gmail.com>
2026-03-26 09:22:46 +0800

    days

--- src/Components/EditCourseForm.tsx
+++ src/Components/EditCourseForm.tsx
@@ -180,6 +180,7 @@ export default function EditCourseForm({ onClose, id } : IProps){
                     <Box className='input-grp w-full '>
                         <select id='numOfDays' onChange={handleSelect} value={courseVal.numOfDays}  className='drop form-input w-full' defaultValue='' required>
                             <option value='' hidden disabled>Select Number of Days</option>
+                            <option value={10}>10 days</option>
                             <option value={6}>6 days</option>
                             <option value={5}>5 days</option>
                             <option value={4}>4 days</option>
