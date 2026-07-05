8588af38abd1bbf88d275509c8478383268b6143
Raphael0025 <rpbisla0025@gmail.com>
2026-07-06 04:59:29 +0800

    MR2

--- src/Components/Page/Training/Analytics/DetailedMR.tsx
+++ src/Components/Page/Training/Analytics/DetailedMR.tsx
@@ -502,7 +502,7 @@ export default function DetailedMonthlyReport ({ batchCourses, filteredTrainList
                 setDbReport({
                     id: savedRecord,
                     generatedAt: Timestamp.now(),
-                    generatedBy: actor,
+                    generatedBy: actor | null,
                 });
     
                 handleToast('Monthly Report Saved!', ``, 3000, 'success');
