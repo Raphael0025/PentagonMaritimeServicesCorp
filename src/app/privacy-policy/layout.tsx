124d321b050447da2a9e8c4108d2ac64bf91013d
Raphael0025 <rpbisla0025@gmail.com>
2026-09-06 12:22:51 +0800

    New Website Update3

--- src/app/privacy-policy/layout.tsx
+++ src/app/privacy-policy/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
     title: 'Privacy Policy',
-    description: "Feel Free to contact us and send your inquiries!",
+    description: " ",
 };
 
 export default function ContactLayout({ children }: { children: React.ReactNode }) {
